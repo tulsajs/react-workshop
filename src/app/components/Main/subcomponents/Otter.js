e85ec232e7a7796e676aee335b2a1417c5271dc7
nearbycoder <nearbycoder@gmail.com>
2016-10-24 22:47:53 -0500

    Show how to get a reference to an input in react

--- src/app/components/Main/subcomponents/Otter.js
+++ src/app/components/Main/subcomponents/Otter.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
 
 export default class Otter extends Component {
+
+  getText () {
+    console.log(this.refs.name.value);
+  }
+
+  // Note the :: operator is just a shortcut for .bind(this)
   render () {
     return (
-      <h3>{this.props.title}</h3>
+      <div>
+        <h3>{this.props.title}</h3>
+        <input type='text' ref='name' />
+        <button type='submit' onClick={::this.getText}> Get Name</button>
+      </div>
     );
   }
 }
