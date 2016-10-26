6f8f8a7db7a4349c06b61d91384085b1540a0436
nearbycoder <nearbycoder@gmail.com>
2016-10-26 15:07:59 -0500

    fix configs

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = (function makeWebpackConfig () {
 
   config.output = {
     path: path.resolve(__dirname, './www'),
-    publicPath: isProd ? '/' : 'http://0.0.0.0:8080/',
+    publicPath: isProd ? '/' : 'http://localhost:8080/',
     filename: isProd ? '[name].[hash].js' : '[name].bundle.js',
     chunkFilename: isProd ? '[name].[hash].js' : '[name].bundle.js'
   };
